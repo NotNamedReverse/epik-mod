91a06057cd598361308b3072b6151b8c6db7b57f
notnamedreverse <Gradyblockmaster@gmail.com>
2023-11-19 20:39:59 -0500

    did

---
+++ src/main/java/com/notnamedreverse/epikmod/EpikMod.java
@@ -0,0 +1,47 @@
+package com.notnamedreverse.epikmod;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(EpikMod.MOD_ID)
+public class EpikMod
+{
+    public static final String MOD_ID = "epikmod";
+
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public EpikMod()
+    {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        modEventBus.addListener(this::commonSetup);
+
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    // yteah
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+
+    }
+
+    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
+    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+    public static class ClientModEvents
+    {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event)
+        {
+            ;
+        }
+    }
+}
