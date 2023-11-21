e3489470137b0eea42ce4a8d6847d642ac73df2f
notnamedreverse <Gradyblockmaster@gmail.com>
2023-11-21 14:04:18 -0500

    scythe test

--- src/main/java/com/notnamedreverse/epikmod/item/ModItems.java
+++ src/main/java/com/notnamedreverse/epikmod/item/ModItems.java
@@ -1,6 +1,7 @@
 package com.notnamedreverse.epikmod.item;
 
 import com.notnamedreverse.epikmod.EpikMod;
+import com.notnamedreverse.epikmod.item.custom.Zircon_scythe;
 import net.minecraft.client.renderer.item.ItemProperties;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
@@ -17,6 +18,9 @@ public class ModItems {
             () -> new Item(new Item.Properties().tab(ModCreativeModeTab.EPIK_TAB)));
     public static final RegistryObject<Item> RAW_ZIRCON = ITEMS.register("raw_zircon",
             () -> new Item(new Item.Properties().tab(ModCreativeModeTab.EPIK_TAB)));
+
+    public static final RegistryObject<Item> ZIRCON_SCYTHE = ITEMS.register("zircon_scythe",
+            () -> new Zircon_scythe(new Item.Properties().tab(ModCreativeModeTab.EPIK_TAB).stacksTo(1)));
 
     public static void register(IEventBus eventBus){
         ITEMS.register(eventBus);
